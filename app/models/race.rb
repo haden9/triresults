5cfe5aa0f4361caf67aafe4597b71d51a26af622
haden9 <haden9@gmail.com>
2016-02-07 16:14:00 -0600

    Correct race_results_spec rq03

--- app/models/race.rb
+++ app/models/race.rb
@@ -12,5 +12,5 @@ class Race
   scope :upcoming, -> {where(:date.gte => Date.current)}
   scope :past, -> {where(:date.lt => Date.current)}
 
-  has_many :entrants, foreign_key: 'race._id', dependent: :delete
+  has_many :entrants, foreign_key: 'race._id', dependent: :delete, order: [:secs.asc, :bib.asc]
 end
