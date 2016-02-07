28fb74b29be70c5c6675bc7398b1e0d820d74a24
haden9 <haden9@gmail.com>
2016-02-07 16:41:17 -0600

    racer_results_spec rq02

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,8 +1,10 @@
 class Racer
   include Mongoid::Document
-  field :info, type: RacerInfo
 
   embeds_one :info, class_name: 'RacerInfo', autobuild: true
+
+  has_many :races, class_name: 'Entrant', dependent: :nullify, foreign_key: 'racer.racer_id',
+    order: :'race.date'.desc
 
   before_create do |racer|
     racer.info.id = racer.id
