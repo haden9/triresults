2b82be35eed0d96c804bfdceff788d47215490e1
haden9 <haden9@gmail.com>
2016-02-07 16:54:16 -0600

    Complete flatten_attributes_spec rq01

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,6 +1,8 @@
 class Racer
   include Mongoid::Document
 
+  ATTRIBUTES = ['first_name', 'last_name', 'gender', 'birth_year', 'city', 'state']
+
   embeds_one :info, class_name: 'RacerInfo', autobuild: true
 
   has_many :races, class_name: 'Entrant', dependent: :nullify, foreign_key: 'racer.racer_id',
@@ -8,5 +10,17 @@ class Racer
 
   before_create do |racer|
     racer.info.id = racer.id
+  end
+
+  ATTRIBUTES.each do |action|
+    define_method("#{action}") do
+      self.info ? self.info.send("#{action}") : nil
+    end
+
+    define_method("#{action}=") do |name|
+      object = self.info ||= RacerInfo.new
+      object.send("#{action}=", name)
+      self.info = object
+    end
   end
 end
--- app/models/racer_info.rb
+++ app/models/racer_info.rb
@@ -14,4 +14,17 @@ class RacerInfo
   validates :gender, inclusion: {in: %w{F M}, message: 'must be M or F'}
   validates :birth_year, numericality: {less_than: Date.current.year, message: 'must be in past'}
 
+  ATTRIBUTES = ['city', 'state']
+
+  ATTRIBUTES.each do |action|
+    define_method("#{action}") do
+      self.residence ? self.residence.send("#{action}") : nil
+    end
+
+    define_method("#{action}=") do |name|
+      object = self.residence ||= Address.new
+      object.send("#{action}=", name)
+      self.residence = object
+    end
+  end
 end
