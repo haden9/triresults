0973c5b387393418d6803dba5c312f05544788f4
haden9 <haden9@gmail.com>
2016-02-07 14:35:54 -0600

    Complete race_results_spec rq02

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -9,6 +9,7 @@ class Entrant
   field :group, type: Placing
 
   embeds_many :results, class_name: 'LegResult', after_add: :update_total
+  embeds_one :race, class_name: 'RaceRef'
 
   def update_total(result)
     self.secs = results.sum(:secs) if result
--- app/models/race_ref.rb
+++ app/models/race_ref.rb
@@ -2,4 +2,6 @@ class RaceRef
   include Mongoid::Document
   field :n, type: String, as: :name
   field :date, type: Date
+
+  embedded_in :entrant
 end
