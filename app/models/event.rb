2e7428905e8fc44f3db4110f456297c545097ca3
haden9 <haden9@gmail.com>
2016-02-06 23:48:49 -0600

    Complete races_spec rq02

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,39 @@
 class Event
   include Mongoid::Document
-  field :order, type: Integer
-  field :name, type: String
-  field :distance, type: Float
-  field :units, type: String
+  field :o, type: Integer, as: :order
+  field :n, type: String, as: :name
+  field :d, type: Float, as: :distance
+  field :u, type: String, as: :units
+
+  embedded_in :parent, polymorphic: true
+
+  def meters
+    case units
+    when 'yards' || 'yard' then
+      distance * 0.9144
+    when 'miles' || 'mile' then
+      distance * 1609.34
+    when 'kilometers' || 'kilometer' then
+      distance * 1000
+    when 'meters' || 'meter' then
+      distance
+    else
+      nil
+    end
+  end
+
+  def miles
+    case units
+    when 'meters' || 'meter' then
+      distance * 0.000621371
+    when 'kilometers' || 'kilometer' then
+      distance * 0.621371
+    when 'yards' || 'yard' then
+      distance * 0.000568182
+    when 'miles' || 'mile' then
+      distance
+    else
+      nil
+    end
+  end
 end
