3043f23ab3b33b7a97b40b87ea18939ede21d7e4
haden9 <haden9@gmail.com>
2016-02-07 18:05:49 -0600

    Complete flatten_attributes_spec rq04

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -19,9 +19,40 @@ class Entrant
 
   attr_accessor :the_race
 
-  embeds_many :results, class_name: 'LegResult', after_add: :update_total
+  embeds_many :results, class_name: 'LegResult', after_add: :update_total, order: :'event.o'.asc
   embeds_one :race, class_name: 'RaceRef', autobuild: true
   embeds_one :racer, as: :parent, class_name: 'RacerInfo', autobuild: true
+
+  RESULTS = {'swim' => SwimResult,
+             't1' => LegResult,
+             'bike' => BikeResult,
+             't2' => LegResult,
+             'run' => RunResult}
+
+  RESULTS.keys.each do |name|
+    define_method("#{name}") do
+      result = results.select {|result| name == result.event.name if result.event}.first
+      unless result
+        result = RESULTS["#{name}"].new(event: {name: name})
+        results << result
+      end
+      result
+    end
+
+    define_method("#{name}=") do |event|
+      event = self.send("#{name}").build_event(event.attributes)
+    end
+
+    RESULTS["#{name}"].attribute_names.reject {|r|/^_/ === r}.each do |prop|
+      define_method("#{name}_#{prop}") do
+        event = self.send(name).send(prop)
+      end
+      define_method("#{name}_#{prop}=") do |value|
+        event = self.send(name).send("#{prop}=",value)
+        update_total nil if /secs/ === prop
+      end
+    end
+  end
 
   def overall_place
     overall.place if overall
@@ -40,7 +71,11 @@ class Entrant
   end
 
   def update_total(result)
-    self.secs = results.sum(:secs) if result
+    sum = 0.0
+    results.each do |r|
+      sum += r.secs if r.secs
+    end
+    self.secs = sum
   end
 
   def the_race
